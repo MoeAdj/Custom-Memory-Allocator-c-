522bc8a36b27b99cd6b9fc159c200635b39ffc7e
MoeAdj <157937026+MoeAdj@users.noreply.github.com>
2025-12-21 17:09:54 -0500

    Define Block structure and allocator functions
    Add structure and function declarations for memory allocator

--- allocator.h
+++ allocator.h
@@ -1,1 +1,17 @@
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
 
+#include <cstddef>
+
+struct Block {
+    size_t size;
+    bool free;
+    Block* next;
+};
+
+void initALLOCATOR();
+void printBlock(Block* b);
+
+
+
+#endif
