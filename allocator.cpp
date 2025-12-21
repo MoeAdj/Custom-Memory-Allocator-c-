13f6192df7431e685226fda3861cb9fe002dcf56
MoeAdj <157937026+MoeAdj@users.noreply.github.com>
2025-12-21 17:21:39 -0500

    Implement basic memory allocator functions

---
+++ allocator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "allocator.h"
+
+cosnt size_t HEAP_SIZE = 1024;
+static char heap[HEAP_SIZE];
+static Block* head = nullptr;
+
+void initALLOCATOR() {
+    head = (Block*)heap;
+    head->size = HEAP_SIZE - sizeof(Block);
+    head->free = true;
+    head->next = nullptr;
+}
+
+void printBlock(Block* b){
+    std::cout <<"Size: " << b->size << std::endl;
+    std::cout <<"free: " << b->free << std::endl;
+    std::cout <<"next: " << b->next << std::endl;
+}
